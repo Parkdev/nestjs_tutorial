dd26b51c31859a80b7bf23e7fc04bde06fbf935d
devPark <devpark@gmail.com>
2024-04-02 14:09:29 +0900

    express middleware 사용해보기

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,32 @@ import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('this is logging middleware');
+  next();
+});
+
+app.get('/cats/som', (req, res, next) => {
+  console.log('this som middleware');
+  next();
+});
+
 app.get('/', (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cat: Cat });
+});
+
+app.get('/cats/blue', (req: express.Request, res: express.Response) => {
+  res.send({ cat: Cat, blue: Cat[0] });
+});
+
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log('this is error middleware');
+  res.send({ error: '404 not found error' });
 });
 
 app.listen(8000, () => {
