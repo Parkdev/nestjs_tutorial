a3280af9e428fe570ed25089ee6efc59274f1f64
devPark <devpark@gmail.com>
2024-04-02 16:44:20 +0900

    Update Delete api 추가

--- dist/app.js
+++ dist/app.js
@@ -2,19 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cat_route_1 = require("./cats/cat.route");
-var app = express();
-app.use(function (req, res, next) {
-    console.log(req.rawHeaders[1]);
-    console.log('this is logging middleware');
-    next();
-});
-app.use(express.json());
-app.use(cat_route_1.default);
-app.use(function (req, res, next) {
-    console.log('this is error middleware');
-    res.send({ error: '404 not found error' });
-});
-app.listen(8000, function () {
-    console.log('Server is running on ...');
-});
+var Server = (function () {
+    function Server() {
+    }
+    Server.prototype.constructore = function () {
+        var app = express();
+        this.app = app;
+    };
+    Server.prototype.setRoute = function () {
+        this.app.use(cat_route_1.default);
+    };
+    Server.prototype.setMiddleware = function () {
+        this.app.use(function (req, res, next) {
+            console.log(req.rawHeaders[1]);
+            console.log('this is logging middleware');
+            next();
+        });
+        this.app.use(express.json());
+        this.setRoute();
+        this.app.use(function (req, res, next) {
+            console.log('this is error middleware');
+            res.send({ error: '404 not found error' });
+        });
+    };
+    Server.prototype.listen = function () {
+        this.setMiddleware();
+        this.app.listen(8000, function () {
+            console.log('Server is running on ...');
+        });
+    };
+    return Server;
+}());
+function init() { }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,48 @@
 import * as express from 'express';
 import catsRouter from './cats/cat.route';
 
-const app: express.Express = express();
+class Server {
+  public app: express.Application;
 
-//* logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log('this is logging middleware');
-  next();
-});
+  constructore() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-//* json middleware
-app.use(express.json());
+  private setRoute() {
+    // 각각의 라우터를 분리하기 위해서 함수로 만들어줌
+    this.app.use(catsRouter);
+  }
 
-//* 라우터 사용
-app.use(catsRouter);
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log('this is logging middleware');
+      next();
+    });
 
-//* 404 middleware
-app.use((req, res, next) => {
-  console.log('this is error middleware');
-  res.send({ error: '404 not found error' });
-});
-app.listen(8000, () => {
-  console.log('Server is running on ...');
-});
+    //* json middleware
+    this.app.use(express.json());
+
+    //* 라우터 사용
+    this.setRoute();
+
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      console.log('this is error middleware');
+      res.send({ error: '404 not found error' });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log('Server is running on ...');
+    });
+  }
+}
+
+function init() {}
+
+// const app: express.Express = express();
